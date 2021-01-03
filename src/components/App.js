ba78577cb8a7c65babb4e0ee005c1f7ee0deb4b3
Williams Blanco <williamsblanco216@gmail.com>
2021-01-03 03:09:11 -0400

    add paginator conditions

--- src/components/App.js
+++ src/components/App.js
@@ -5,19 +5,33 @@ import ListImgs from './ListImgs';
 const App = () => {
   const [search, setSearch] = useState('');
   const [images, setImages] = useState([]);
+  const [currentPages, setCurrentPages] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
   useEffect(() => {
     if(!search) return
     const requestApi = async () => {
       const numberOfResult = '30'
       const API_KEY = '15430207-b4af63f91ba4c8a3b2091efb2'
-      const URL= `https://pixabay.com/api/?key=${API_KEY}&q=&per_page=${numberOfResult}`
+      const URL= `https://pixabay.com/api/?key=${API_KEY}&q=&per_page=${numberOfResult}&page=${currentPages}`
 
       const request = await fetch(URL)
       const response = await request.json()
+
+      setTotalPages(Math.ceil(response.hits/numberOfResult))
       setImages(response.hits)
     }
     requestApi()
-  }, [search]);
+  }, [search, currentPages]);
+
+  const previousPages = e => {
+    if(currentPages < 1) return
+    setCurrentPages(state => state - 1)
+  }
+
+  const nextPages = e => {
+    if(currentPages > totalPages) return
+    setCurrentPages(state => state + 1)
+  }
 
   return (
     <div className="container">
@@ -29,6 +43,29 @@ const App = () => {
       </div>
       <div className="row justify-content-center">
         <ListImgs images={images}/>
+
+        {
+          currentPages != 1 &&
+          <button
+          type="button"
+          className="bbtn btn-info mr-1"
+          onClick={previousPages}
+          >
+           &laquo; Anterior 
+          </button>
+        }
+
+        {
+          currentPages != totalPages &&
+          <button
+            type="button"
+            className="bbtn btn-info mr-1"
+            onClick={nextPages}
+          >
+            Siguiente &raquo;
+          </button>
+        }
+
       </div>
     </div>
   );
